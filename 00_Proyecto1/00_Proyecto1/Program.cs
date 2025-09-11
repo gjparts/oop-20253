614a560d98ea5b9ea110d4b74dffcc1d8ab4d4b3
GJParts <39961515+gjparts@users.noreply.github.com>
2025-09-11 09:51:57 -0600

    commit 1109202524050

--- 00_Proyecto1/00_Proyecto1/Program.cs
+++ 00_Proyecto1/00_Proyecto1/Program.cs
@@ -46,6 +46,97 @@
             Console.WriteLine("Fondo verde letra azul");
 
             Console.ResetColor();
+
+            //se puede usar diferentes colores en una sola linea
+            //por medio de Write
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Kiwi");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Naranja");
+            Console.BackgroundColor  = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Objetos");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Sandia");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            //para poder imprimir multiples lineas en un solo String
+            //en C# debe poner el caracter @ antes del String, esto tambien
+            //les permite ignorar los caracteres de escape.
+            Console.WriteLine(@"
+        _   ,_,   _
+       / `'=) (='` \
+      /.-.-.\ /.-.-.\ 
+      `      ""      `
+");
+
+            //hacer una pausa
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey(true);
+
+            //limpiar la pantalla
+            Console.Clear();
+
+            //establecer en que posicion de la pantalla va a imprimir.
+            Console.SetCursorPosition(70, 3);
+            Console.WriteLine("Texto en columna 70, linea 3");
+
+            Console.SetCursorPosition(10, 0);
+            Console.WriteLine("Texto en columna 10, linea 0");
+
+            //importante: la ultima linea en que la se imprime es donde quedara el cursor
+            //para futuras impresiones.
+
+            Console.SetCursorPosition(30, 7);
+            Console.WriteLine("Texto random");
+
+            //hacer una pausa
+            Console.WriteLine("Presione cualquier tecla para continuar...");
+            Console.ReadKey(true);
+
+            //limpiar la pantalla
+            Console.Clear();
+
+            //variables
+            int x = 5;
+            double dd = 3.1416;
+            float ff = 2.25f;
+            long num = 1234567891234561L;
+
+            //caza de errores
+            try
+            {
+                //captura de datos en variables
+                int a;
+                Console.Write("Digite un entero: ");
+                a = int.Parse(Console.ReadLine());
+
+                double d1;
+                Console.Write("Digite un double: ");
+                d1 = double.Parse(Console.ReadLine());
+
+                float f1;
+                Console.Write("Digite un float: ");
+                f1 = float.Parse(Console.ReadLine());
+
+                long l1;
+                Console.Write("Digite un long: ");
+                l1 = long.Parse(Console.ReadLine());
+
+                //imprimir variables
+                //concatenar de forma tradicional:
+                Console.WriteLine("El valor de a es "+a);
+                //concatenar usando sintaxis de interpolacion (signo de dolar antes del String)
+                Console.WriteLine($"El valor de a es {a} y el valor de d1 es {d1}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Debe ingresar solo numeros");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- 00_Proyecto1/00_Proyecto1/obj/Debug/net8.0/00_Proyecto1.AssemblyInfo.cs
+++ 00_Proyecto1/00_Proyecto1/obj/Debug/net8.0/00_Proyecto1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("00_Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb61fa756ddde09232604f060746ccd12cd7dbcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae4d6f5e899e16429ec2354bcb615ebc9bab82b")]
 [assembly: System.Reflection.AssemblyProductAttribute("00_Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("00_Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
