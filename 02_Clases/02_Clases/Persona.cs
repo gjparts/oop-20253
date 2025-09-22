70546f52469e590b04f4c010b1c70be3ffacd26d
GJParts <39961515+gjparts@users.noreply.github.com>
2025-09-22 10:58:12 -0600

    commit 2209202527681

---
+++ 02_Clases/02_Clases/Persona.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Clases
+{
+    public class Persona
+    {
+        //Propiedades
+        public String Nombre { get; set; }
+        public int Edad { get; set; }
+        public char Genero { get; set; }
+        public String DNI { get; set; }
+        /*Constructor:
+        Es un metodo que devuelve memoria para una instancia de una clase
+        -> Un constructor se puede usar para inicializar las propiedades
+        -> Se recomienda que sean publicos
+        -> Pueden tener o no parametros
+        -> en C# siempre se llaman igual que la clase
+        -> No tienen tipo de dato puesto que devuelven una instancia de la clase (objeto)
+        -> en C# una clase puede tener varios constructores siempre y cuando
+           estos tenga diferente firma
+        -> En C# si no define ningun constructor entonces se asume que la clase tiene
+           un constructor sin parametros
+        -> Los constructores no siempre se usan para inicializar Propiedades, tambien
+           se pueden usar para disparar eventos, conexiones o acciones.*/
+
+        public Persona()
+        {
+            //Constructor sin parametros
+            this.Nombre = "no tiene";
+            this.Edad = 0;
+            this.Genero = 'X';
+            this.DNI = "no definido";
+        }
+        public Persona(String nombre, int edad)
+        {
+            //constructor con dos parametros
+            //en C# se recomienda usar camel Case para nombrar los parametros de metodo
+            //en camel Case la primer letra es minuscula y las siguiente palabra
+            //va en mayuscula: ciudadOrigen  nombreDelConyuge  salarioMensualEmpleado
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Genero = 'X';
+            this.DNI = "no definido";
+        }
+        public Persona(String nombre, int edad, char genero, String dni)
+        {
+            //constructor con todos los parametros
+            this.Nombre= nombre;
+            this.Edad = edad;
+            this.Genero = genero;
+            this.DNI = dni;
+        }
+
+        //Metodos (llevan tipo de dato)
+        public void Imprimir()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("********** Perfil de Persona **********");
+            Console.WriteLine($"Nombre: {this.Nombre}");
+            Console.WriteLine($"Edad: {this.Edad}");
+            Console.WriteLine($"Genero: {this.GetGeneroDescripcion()}");
+            Console.WriteLine($"DNI: {this.DNI}");
+            Console.WriteLine($"Es mayor de edad: {this.EsMayorDeEdad()}");
+            Console.ResetColor();
+        }
+        public bool EsMayorDeEdad()
+        {
+            if (this.Edad >= 18)
+                return true;
+            else
+                return false;
+        }
+        public String GetGeneroDescripcion()
+        {
+            if (this.Genero == 'M') return "Masculino";
+            if (this.Genero == 'F') return "Femenino";
+            if (this.Genero == 'X') return "Otros";
+            return "No configurado en el programa.";
+        }
+    }
+}
--- 02_Clases/02_Clases/Program.cs
+++ 02_Clases/02_Clases/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //en C# cuando una clase no tiene constructor definido
+            //el lenguaje le asigna un constructor sin parametros
+            //que unicamente devuelve el objeto en determinada direccion
+            //de memoria
+            Mascota ma1 = new Mascota();
+            Console.WriteLine("Valores de las propiedades de ma1:");
+            Console.WriteLine("Nombre: "+ma1.Nombre);
+            Console.WriteLine($"Especie: {ma1.Especie}");
+            Console.WriteLine($"Edad: {ma1.Edad}");
+            Console.WriteLine($"Genero: {ma1.Genero}");
+            Console.WriteLine($"Direccion hash de ma1: {ma1.GetHashCode()}");
+
+            Mascota ma2 = new Mascota();
+            //colocar manualmente los valores de las propiedades:
+            ma2.Nombre = "Chispita";
+            ma2.Especie = "Gato";
+            ma2.Edad = 2;
+            ma2.Genero = 'H';
+            Console.WriteLine("*************************************");
+            Console.WriteLine("Nombre: " + ma2.Nombre);
+            Console.WriteLine($"Especie: {ma2.Especie}");
+            Console.WriteLine($"Edad: {ma2.Edad}");
+            Console.WriteLine($"Genero: {ma2.Genero}");
+            Console.WriteLine($"Direccion hash de ma2: {ma2.GetHashCode()}");
+
+            Console.WriteLine("***********************************");
+            Persona per1 = new Persona();
+            per1.Imprimir();
+
+            Persona per2 = new Persona("Gerardo",42);
+            per2.Imprimir();
+
+            Persona per3 = new Persona("Filomeno Colinas", 77, 'M', "1111111111111");
+            per3.Imprimir();
         }
     }
 }
