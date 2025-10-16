756d0dc33c764e489f1db1a8f348d114c639c2ca
GJParts <39961515+gjparts@users.noreply.github.com>
2025-10-16 09:58:44 -0600

    commit 161020254552

---
+++ 07_Multiplicidad/06_AsociacionClases/Factura.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_AsociacionClases
+{
+    public class Factura
+    {
+        //Campos privados
+        private Producto[] _productos;
+        //_productos es un Arreglo de elementos de clase Producto
+
+        //Propiedades
+        public int Numero {  get; set; }
+        public DateTime Fecha { get; set; }
+        public Cliente Cliente { get; set; } //Asoc. por Agregacion
+        public Producto[] Productos
+        {
+            get => this._productos;
+            set
+            {
+                //el arreglo no puede ser null
+                if (value == null)
+                    throw new ArgumentException("Productos en Factura no puede ser null");
+                else
+                {
+                    //el arreglo debe tener al menos un item
+                    if (value.Length == 0)
+                        throw new ArgumentException("Productos debe tener al menos un item");
+                    else
+                    {
+                        //item 0 no puede venir null
+                        if (value[0] == null)
+                            throw new ArgumentException("Elemento 0 en Productos no puede ser null");
+                        else
+                            this._productos = value; //se acepta
+                    }
+                }
+            }
+        }
+
+        //Constructor
+        public Factura(int numero, DateTime fecha, Cliente cliente, Producto[] productos)
+        {
+            this.Numero = numero;
+            this.Fecha = fecha;
+            this.Cliente = cliente;
+            this.Productos = productos;
+        }
+
+        //Metodos
+        public void Imprimir()
+        {
+            Console.WriteLine("*** Factura ***");
+            Console.WriteLine($"Numero: {this.Numero}");
+            //Fecha en formato yyyy/MM/dd
+            Console.WriteLine($"Fecha: {this.Fecha.ToString("yyyy/MM/dd")}");
+
+            //Cliente viene asociado por agregacion, por lo tanto
+            //puede llegar a ser null.
+            if( this.Cliente == null )
+                Console.WriteLine("Cliente: Consumidor final");
+            else
+            {
+                Console.WriteLine($"Cliente: {this.Cliente.Nombre}");
+                Console.WriteLine($"RTN: {this.Cliente.Rtn}");
+            }
+
+            //tablita de productos, Producto1 nunca a venir null
+            //en cambio Producto2 al 4 si pueden venir null
+            //por lo tanto no se imprimen en caso de que vengan null
+            float suma = 0.00f; //variable acumuladora
+            Console.WriteLine("producto\tprecio");
+            
+            //recorrer cada item del arreglo de productos
+            foreach( Producto item in this.Productos)
+            {
+                //ignorar cualquier item que sea null
+                if( item != null)
+                {
+                    Console.WriteLine($"{item.Nombre}\t{item.PrecioVenta}");
+                    suma += item.PrecioVenta; //sumar precio del producto al subtotal
+                }
+            }
+
+            //Resultado redondeado a dos decimales (Math.Round)
+            Console.WriteLine($"Subtotal: {Math.Round(suma,2)}");
+            Console.WriteLine($"ISV 15%: {Math.Round(suma*0.15, 2)}");
+            Console.WriteLine($"Total: {Math.Round(suma * 1.15, 2)}");
+        }
+    }
+}
---
+++ 07_Multiplicidad/06_AsociacionClases/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _06_AsociacionClases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cliente c1 = new Cliente("Gerardo Portillo","123","1234-1234","");
+                Cliente c2 = new Cliente("Mario Verde", "777777", "1111-1111", "");
+
+                CategoriaProducto cat1 = new CategoriaProducto("Bebidas", "18");
+                CategoriaProducto cat2 = new CategoriaProducto("Verduras y Frutas", "1");
+                CategoriaProducto cat3 = new CategoriaProducto("Churros", "8");
+
+                Producto p1 = new Producto("Coca Cola 3L",cat1,65);
+                Producto p2 = new Producto("Tomate LB", cat2, 15);
+                Producto p3 = new Producto("Papa LB", cat2, 14);
+                Producto p4 = new Producto("Zambos Grande", cat3, 45);
+                Producto p5 = new Producto("Rica Sula", cat3, 8);
+
+                Producto[] prods1 = { p2 };
+                Factura f1 = new Factura(1, DateTime.Now, c1, prods1);
+                f1.Imprimir();
+
+                Producto[] prods2 = { p1, p5, p4, p4 };
+                DateTime fecha1 = new DateTime(2025, 9, 15);
+                Factura f2 = new Factura(2, fecha1, null, prods2);
+                f2.Imprimir();
+
+                Producto[] prods3 = { p3, p2 };
+                Factura f3 = new Factura(3, new DateTime(2025, 8, 2), c2, prods3);
+                f3.Imprimir();
+
+                Producto[] prods4 = { p2, p1, p1, p5, p4, p2, p2, p5, p1, p1 };
+                Factura f4 = new Factura(4, DateTime.Now, c1, prods4);
+                f4.Imprimir();
+
+                //esto da error porque viola la regla de que el item 0 en Productos no debe ser null
+                /*
+                Producto[] prods5 = { null, p1, p2 };
+                Factura f5 = new Factura(5, DateTime.Now, c2, prods5);
+                f5.Imprimir();
+                */
+
+                //la coleccion de Productos no puede ser null
+                /*
+                Factura f6 = new Factura(6, DateTime.Now, c2, null);
+                f6.Imprimir();
+                */
+
+                //la coleccion de Productos debe traer al menos un producto
+                /*
+                Producto[] prods7 = { };
+                Factura f7 = new Factura(7, DateTime.Now, c2, prods7);
+                */
+
+                //es posible instanciar un arreglo directamente en el constructor
+                //sin necesidad de una variable
+                Factura f8 = new Factura(8, DateTime.Now, c2,new Producto[] { p5, p2, p1, p1, p3, p1, p1 } );
+                f8.Imprimir();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
